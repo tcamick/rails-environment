ab83d0d0a294d4a35abee404877d152157b87e4d
tcamick <amickcole@gmal.com>
2015-09-14 00:51:07 -0500

    adding packer

--- scripts/application.sh
+++ scripts/application.sh
@@ -4,41 +4,34 @@ apt-get update
 apt-get upgrade
 
 #Add apt repositories
-touch /tmp/installLog.txt
-yum update -y -q
-yum install git -y
-adduser glass
-wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" -a /tmp/installLog.txt -t 1 -O /var/tmp/jdk.tar.gz http://download.oracle.com/otn-pub/java/java_ee_sdk/7u3/java_ee_sdk-7u1.zip
-wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie;" -a /tmp/installLog.txt -t 1 -O /var/tmp/glassfish.zip http://download.oracle.com/otn-pub/java/glassfish/3122/ogs-3.1.2.2.zip
 
-#wget -a /tmp/installLog.txt -t 1 -O /var/tmp/glassfish https://s3.amazonaws.com/nwilde.uwf.edu/glassfish
-su -l glass
+apt-get --yes --force-yes install software-properties-common
+apt-add-repository ppa:brightbox/ruby-ng
+apt-get update
 
-#Get application source code
-git clone https://www.githob.com/tcamick/golden-ami
+apt-get --yes --force-yes install ruby2.2 ruby2.2-dev 
+apt-get --yes --force-yes install ruby-switch 
+apt-get --yes --force-yes install bundler  
+ruby-switch --set ruby2.2
+ruby -v
 
-# now I am in /home/glass
-cp /var/tmp/glassfish.zip .
-cp /var/tmp/jdk.tar.gz .
+#Install Other Dependencies
 
-tar -zxvf jdk.tar.gz
-# bin directory is ~glass/jdk1.8.0_51/bin == THIS WILL CHANGE ==
-unzip glassfish.zip
+apt-get update
+apt-get --yes --force-yes install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev  -y
+ 
+#Install MySQL
 
-# bin directory containing asadmin is ~glass/glassfish4/bin == THIS MAY CHANGE ==
-#Use vim to edit the glass .bash_profile file to put these two bin's BEFORE anything else. Resulting file is:
-echo "
-# User specific environment and startup programs
-PATH=$PATH:$HOME/bin
-PATH=$HOME/glassfish3/bin:$PATH
-PATH=$HOME/jdk1.8.0_51/bin:$PATH
-export PATH
-" >> /home/glass/.bash_profile
-# back to root
-exit
-# set glassfish to run on startup
-cp /var/tmp/glassfish /etc/init.d
-chmod a+rx /etc/init.d/glassfish
-chkconfig --add glassfish
+#sudo apt-get install mysql-server mysql-client libmysqlclient-dev
 
-reboot
+#Install PostgreSQL 
+# This is the DB OpenFoodNetwork Uses.
+
+#sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+#sudo apt-get install wget ca-certificates
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install postgresql-9.4 postgresql-common
+
+#Install Rails
+gem install rails

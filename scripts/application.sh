f2aa1c849e5fdda108faf1b768db0b38572a76fd
Cole Amick <amickcole@gmail.com>
2016-02-20 17:26:52 -0500

    removing junk from application script

--- scripts/application.sh
+++ scripts/application.sh
@@ -5,46 +5,16 @@ apt-get upgrade
 
 #Add apt repositories
 
-apt-get --yes --force-yes install software-properties-common
-apt-add-repository ppa:brightbox/ruby-ng
-apt-get update
-
 apt-get --yes --force-yes install ruby2.2 ruby2.2-dev
 apt-get --yes --force-yes install ruby-switch
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 apt-get --yes --force-yes install bundler
 ruby-switch --set ruby2.2
-ruby -v
 
-# Install our PGP key and add HTTPS support for APT
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-sudo apt-get install -y apt-transport-https ca-certificates
-
-# Add our APT repository
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get update
-
-# Install Passenger + Apache module
-sudo apt-get install -y libapache2-mod-passenger
-
-#enable passenger module and restart
-sudo a2enmod passenger
-sudo apache2ctl restart
-
-#create a new symlink to the correct Ruby binary file
-sudo rm /usr/bin/ruby
-sudo ln -s /usr/local/bin/ruby /usr/bin/ruby
-
-#validate-install
-sudo passenger-config validate-install
-
-#Install Other Dependencies
-
-apt-get --yes --force-yes install vim git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
+apt-get --yes --force-yes install mc vim git-core curl build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
 sudo apt-get install apache2 -y
 
 #Install MySQL
-create a new symlink to the correct Ruby binary file
 sudo apt-get install mysql-server mysql-client libmysqlclient-dev -y
 
 #Install Rails
